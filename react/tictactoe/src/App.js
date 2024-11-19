97ddff7ca9f93f31fa6061723fc9ea96b76762f6
minhdang118 <mdang2@u.rochester.edu>
2024-11-19 11:01:22 +0700

    refining repetitive components

--- react/tictactoe/src/App.js
+++ react/tictactoe/src/App.js
@@ -8,6 +8,12 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function SquareGen({ squareIdx }) {
+  return (
+    <Square value={squares[squareIdx]} onSquareClick={() => handleClick(squareIdx)} />
+  )
+}
+
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
@@ -30,23 +36,27 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  const row0 = [0, 1, 2]
+  const row1 = [3, 4, 5]
+  const row2 = [6, 7, 8]
+
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        {row0.map((squareIdx) => (
+          <Square value={squares[squareIdx]} onSquareClick={() => handleClick(squareIdx)} />
+        ))}
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        {row1.map((squareIdx) => (
+          <Square value={squares[squareIdx]} onSquareClick={() => handleClick(squareIdx)} />
+        ))}
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        {row2.map((squareIdx) => (
+          <Square value={squares[squareIdx]} onSquareClick={() => handleClick(squareIdx)} />
+        ))}
       </div>
     </>
   );
@@ -105,11 +115,12 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+
+  const winner = lines.filter(
+    ([a, b, c]) => squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+  )
+  if (winner.length > 0) {
+    return squares[winner[0][0]];
   }
   return null;
 }
